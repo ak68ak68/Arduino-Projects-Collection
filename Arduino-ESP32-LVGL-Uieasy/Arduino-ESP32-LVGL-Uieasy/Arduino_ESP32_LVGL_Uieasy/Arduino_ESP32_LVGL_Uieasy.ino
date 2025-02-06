d40ed9dea530a0e37674cd5ccf67ee838fb69a0f
ak68ak68 <303760886@qq.com>
2025-02-06 13:29:26 +0800

    esp32画圆

--- Arduino-ESP32-LVGL-Uieasy/Arduino-ESP32-LVGL-Uieasy/Arduino_ESP32_LVGL_Uieasy/Arduino_ESP32_LVGL_Uieasy.ino
+++ Arduino-ESP32-LVGL-Uieasy/Arduino-ESP32-LVGL-Uieasy/Arduino_ESP32_LVGL_Uieasy/Arduino_ESP32_LVGL_Uieasy.ino
@@ -1,56 +1,23 @@
-#include <Arduino.h>
 #include <U8g2lib.h>
-#include <Wire.h>
 
-// 使用 I2C 接口初始化 U8g2 对象，适用于 128x64 的 SSD1306 OLED 屏幕
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
-
-// 绘制心形函数
-void drawHeart(u8g2_uint_t x, u8g2_uint_t y, u8g2_uint_t size) {
-  for (float t = 0; t <= 2 * PI; t += 0.01) {
-    // 心形的参数方程
-    float xCoord = 16 * pow(sin(t), 3);
-    float yCoord = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t);
-
-    // 调整坐标和比例
-    int drawX = x + static_cast<int>(xCoord * size / 20);
-    int drawY = y - static_cast<int>(yCoord * size / 20);
-
-    // 绘制点
-    u8g2.drawPixel(drawX, drawY);
-  }
-}
+// 修正类型名称的大小写
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 22, /* data=*/ 21, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
-  // 初始化 U8g2 对象
+  // 初始化U8g2库
   u8g2.begin();
-  // 设置对比度，值范围为 0 - 255
-  u8g2.setContrast(255);
 }
 
 void loop() {
   // 清空屏幕
   u8g2.firstPage();
   do {
-    // 设置字体
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-
-    // 计算屏幕中心坐标
-    int centerX = 64;
-    int centerY = 32;
-
-    // 定义心形大小
-    int heartSize = 20;
-
-    // 计算心形的起始坐标，使心形居中
-    int startX = centerX - heartSize;
-    int startY = centerY - heartSize;
-
-    // 绘制一个心形，位置在屏幕中心，大小为 20
-    drawHeart(startX, startY, heartSize);
-
+    // 设置绘图颜色为白色
+    u8g2.setDrawColor(1);
+    // 绘制圆形，参数依次为圆心x坐标、圆心y坐标、半径、绘制模式（U8G2_DRAW_ALL表示绘制整个圆形）
+    u8g2.drawCircle(64, 32, 20, U8G2_DRAW_ALL);
   } while (u8g2.nextPage());
 
-  // 延迟一段时间，可根据需求调整
+  // 可以根据需要添加延迟
   delay(1000);
 }

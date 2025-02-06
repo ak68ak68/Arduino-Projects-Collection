f4c2de7a79630f617bdfc4b65e9b3d71bece22f8
ak68ak68 <303760886@qq.com>
2025-02-07 01:43:14 +0800

    简单UIu6g2

--- Arduino-ESP32-LVGL-Uieasy/Arduino-ESP32-LVGL-Uieasy/Arduino_ESP32_LVGL_Uieasy/Arduino_ESP32_LVGL_Uieasy.ino
+++ Arduino-ESP32-LVGL-Uieasy/Arduino-ESP32-LVGL-Uieasy/Arduino_ESP32_LVGL_Uieasy/Arduino_ESP32_LVGL_Uieasy.ino
@@ -1,23 +1,37 @@
+#include <Arduino.h>
 #include <U8g2lib.h>
+#include <Wire.h>
 
-// 修正类型名称的大小写
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 22, /* data=*/ 21, /* reset=*/ U8X8_PIN_NONE);
+// 创建 U8g2 对象，注意类型名称的大小写
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
-  // 初始化U8g2库
-  u8g2.begin();
+    // 初始化 U8g2
+    u8g2.begin();
 }
 
 void loop() {
-  // 清空屏幕
-  u8g2.firstPage();
-  do {
-    // 设置绘图颜色为白色
-    u8g2.setDrawColor(1);
-    // 绘制圆形，参数依次为圆心x坐标、圆心y坐标、半径、绘制模式（U8G2_DRAW_ALL表示绘制整个圆形）
-    u8g2.drawCircle(64, 32, 20, U8G2_DRAW_ALL);
-  } while (u8g2.nextPage());
+    // 清除屏幕
+    u8g2.firstPage();
+    do {
+        // 设置字体
+        u8g2.setFont(u8g2_font_ncenB08_tr);
 
-  // 可以根据需要添加延迟
-  delay(1000);
+        // 在屏幕上绘制文本
+        u8g2.drawStr(10, 20, "Simple UI Example");
+
+        // 绘制一条横线
+        u8g2.drawHLine(10, 30, 100);
+
+        // 绘制一个矩形
+        u8g2.drawFrame(10, 40, 50, 20);
+
+        // 在矩形内绘制文本
+        u8g2.setFont(u8g2_font_ncenB08_tr);
+        u8g2.drawStr(20, 53, "Box");
+
+    } while (u8g2.nextPage());
+
+    // 可以根据需要添加延时
+    delay(2000);
 }
